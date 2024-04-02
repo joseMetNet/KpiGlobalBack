93015a1a93c182453b5b44e897a5dfba370e1390
Efrain Palacios <efpalaciosmo@unal.edu.co>
2024-04-02 10:44:17 -0500

    change scheme of database to test

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,16 +3,17 @@ import 'colors';
 import { config } from './config';
 
 export const sequelize = new Sequelize(config.DATABASE_NAME, config.DATABASE_USER, config.DATABASE_PASSWORD, {
-	dialect: 'mssql',
-	host: config.DATABASE_SERVER,
-	port: config.DATABASE_PORT, // Default port
-	dialectOptions: {
-		instanceName: 'SQLEXPRESS',
-		requestTimeout: 30000
-	},
-	pool: {
-		max: 50,
-		min: 0,
-		idle: 10000
-	}
+  dialect: 'mssql',
+  host: config.DATABASE_SERVER,
+  schema: 'test',
+  port: config.DATABASE_PORT, // Default port
+  dialectOptions: {
+    instanceName: 'SQLEXPRESS',
+    requestTimeout: 30000
+  },
+  pool: {
+    max: 50,
+    min: 0,
+    idle: 10000
+  }
 });
